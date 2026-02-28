175f13141746af3952f48977531c61ac222b78e7
codeBodger <diesel.ack+github@gmail.com>
2026-02-28 15:28:48 -0500

    Wrote async getArticleCategories

--- getArticle.js
+++ getArticle.js
@@ -17,3 +17,23 @@ async function getArticle(title) {
                 .then((x) => (ret = x));
         return ret;
 }
+
+async function getArticleCategories(title) {
+        const paramsObj = {
+                action: "query",
+                prop: "categories",
+                titles: title,
+                format: "json",
+                cllimit: "max",
+                origin: "*"
+        };
+        const params = new URLSearchParams(paramsObj);
+        let ret = {};
+        await fetch(`https://en.wikipedia.org/w/api.php?${params.toString()}`)
+                .then((x) => (x.json()))
+                .then((x) => (x.query.pages))
+                .then((x) => (x[Object.keys(x)[0]]))
+                .then((x) => (x.categories.map((y) => (y.title))))
+                .then((x) => (ret = x));
+        return ret;
+}
