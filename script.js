4ac3c429362d955d48cae528bd1938adb41309dd
Ethan Rigor <ethanrig@udel.edu>
2026-02-28 17:06:28 -0500

    put into new file

--- script.js
+++ script.js
@@ -21,24 +21,7 @@
 
     
     // ""FUNCTIONS"""
-    var userGuesses = []
-    var guessesCount = 0
-    $( "#guessForm" ).on( "submit", function(e) {
-        e.preventDefault();
-        var input = $("#guessInput").val()
-        if(userGuesses.includes(String(input))){
-          $("#message").text("You've already used: " + String(input)+ "; You've guessed "+guessesCount+" times!")
-        }
-        else{
-          guessesCount+=1
-          $("#message").text("You've guessed "+guessesCount+" times!")          
-          userGuesses.push(input);
-        }
-        console.log(guessesCount)
-        console.log(input)
-        console.log(userGuesses)
-    });
-
+    
   });
   
 })(jQuery);
