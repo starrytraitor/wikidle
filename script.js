b0376764982795e7e0e69e8c2c7fca84583dad42
starrytraitor <ssszhang05@gmail.com>
2026-02-28 14:57:28 -0500

    testing form

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
   // We use some Javascript and the URL #fragment to hide/show different parts of the page
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
   $(window).on('load hashchange', function(){
+
     
     // First hide all content regions, then show the content-region specified in the URL hash 
     // (or if no hash URL is found, default to first menu item)
@@ -17,7 +18,19 @@
     
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+
     
+    // ""FUNCTIONS"""
+    var userGuesses = []
+    $( "#guessForm" ).on( "submit", function(e) {
+        e.preventDefault();
+        alert("Handler for 'submit' called.");
+
+        var input = $("#guessInput").val()
+        userGuesses.push();
+        print(userGuesses)
+    });
+
   });
   
 })(jQuery);
