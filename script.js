047e35a9cc4b2422e2c11dd950a04c8c52e1131b
Ethan Rigor <ethanrig@udel.edu>
2026-02-28 17:04:06 -0500

    added guess count / prevent repeat guesses

--- script.js
+++ script.js
@@ -22,11 +22,19 @@
     
     // ""FUNCTIONS"""
     var userGuesses = []
+    var guessesCount = 0
     $( "#guessForm" ).on( "submit", function(e) {
         e.preventDefault();
-
         var input = $("#guessInput").val()
-        userGuesses.push(input);
+        if(userGuesses.includes(String(input))){
+          $("#message").text("You've already used: " + String(input)+ "; You've guessed "+guessesCount+" times!")
+        }
+        else{
+          guessesCount+=1
+          $("#message").text("You've guessed "+guessesCount+" times!")          
+          userGuesses.push(input);
+        }
+        console.log(guessesCount)
         console.log(input)
         console.log(userGuesses)
     });
