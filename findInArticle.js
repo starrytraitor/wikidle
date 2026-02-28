fcfa161690857c2eac6cfee50ea504b90a80de0d
HenryLeap <henrywleap@gmail.com>
2026-02-28 16:00:33 -0500

    Changed getArticle to findInArticle, fixed finding word in middle of word

--- findInArticle.js
+++ findInArticle.js
@@ -1,5 +1,10 @@
-
-export function findInputInArticle(input, article){
+/**
+ * 
+ * @param {string} input the user's input
+ * @param {Object} article the wikipedia article
+ * @returns array containing the indices of each occurence of the input. [-1] if the input is not in the article, [-2] if the input is the title of the article
+ */
+function findInArticle(input, article){
 	//returns a list of indexes. The input is found at each index
 	//returns -1 if the word is not in the article
 	//returns -2 if the word is the title
@@ -15,16 +20,12 @@ export function findInputInArticle(input, article){
 	while(!isDone){
 		inputIndex=body.indexOf(input,currentIndex);
 		if(inputIndex!=-1){
-			// if(body.charAt(inputIndex-2)=='='){
-			// }
-			if()
+			if(!body[inputIndex-1].match(/\w/) && !body[inputIndex+input.length].match(/\w/)){
 				wordLocations.push(inputIndex);
-				currentIndex=inputIndex+input.length;
+			}
+			currentIndex=inputIndex+input.length;
 		} else{isDone=true}
 	}
 	return wordLocations;
 }
-const article= getArticle(/*articleTitle*/);
-const userInput = getUserInput();
-findInputInArticle(userInput,article)
 
