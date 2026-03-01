8795c9b9bf5980eeb4501e9d6315bb0c6dafcd27
codeBodger <diesel.ack+github@gmail.com>
2026-03-01 04:25:43 -0500

    Removed title from own p tag

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 (function ($) {
-  
+
   // We use some Javascript and the URL #fragment to hide/show different parts of the page
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
   $(window).on('load hashchange', function(){
 
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
+
+    // First hide all content regions, then show the content-region specified in the URL hash
     // (or if no hash URL is found, default to first menu item)
     $('.content-region').hide();
-    
+
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
+
     // Now show the region specified in the URL hash
     $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
-    
+    // Highlight the menu link associated with this region by adding the .active CSS class
+    $('.main-menu a[href="'+ region +'"]').addClass('active');
+
+
     // ""FUNCTIONS""
     const articleTitle = ARTICLE_LIST[Math.floor(Math.random() * ARTICLE_LIST.length)];
-    getArticle(articleTitle).then((result => { $("#articleContents").html(result.body),$("#articleName").html(result.title)}));
-    
+    getArticle(articleTitle).then((result => { $("#articleContents").html(result.body) }));
+
 
     let userGuesses = []
     $( "#guessForm" ).on( "submit", function(e) {
@@ -38,5 +38,5 @@
     });
 
   });
-  
+
 })(jQuery);
