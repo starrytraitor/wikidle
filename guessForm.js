5f9dc6908687c990f8b3bb848a93c5c972a07b43
Ethan Rigor <ethanrig@udel.edu>
2026-02-28 19:45:53 -0500

    disapear fix

--- guessForm.js
+++ guessForm.js
@@ -4,14 +4,13 @@ var userGuesses = []
         e.preventDefault();
         var input = $("#guessInput").val()
         if(userGuesses.includes(String(input))){
-          $("#message").text("You've already used: " + String(input)+ "; You've guessed "+guessesCount+" times!")
+          $("#userMessage").text("You've already used: " + String(input)+ "; You've guessed "+guessesCount+" times!")
         }
         else{
           guessesCount+=1
-          $("#message").text("You've guessed "+guessesCount+" times!")          
+          $("#userMessage").text("You've guessed "+guessesCount+" times!")          
           userGuesses.push(input);
         }
-        console.log(guessesCount)
         console.log(input)
         console.log(userGuesses)
     });
