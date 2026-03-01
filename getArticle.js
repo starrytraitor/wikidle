053b4bc5cdd45b5ea557b906b690ea67fb338b0a
codeBodger <diesel.ack+github@gmail.com>
2026-03-01 04:20:22 -0500

    Put title where/how needs to be

--- getArticle.js
+++ getArticle.js
@@ -13,7 +13,8 @@ async function getArticle(title) {
                 .then((x) => (x.json()))
                 .then((x) => (x.query.pages))
                 .then((x) => (x[Object.keys(x)[0]]))
-                .then((x) => ({title: `=\n${x.title}\n=\n\n`, body: x.extract}))
+                .then((x) => ({title: x.title,
+                               body: `= ${x.title} =\n\n${x.extract}`}))
                 .then((x) => (ret = x));
         return ret;
 }
